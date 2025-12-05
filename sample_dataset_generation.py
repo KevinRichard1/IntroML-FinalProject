876c9c48bd5a1a4580cecafa11a893d6c22ce126
Kevin <kevin.rohit@gmail.com>
2025-12-04 21:08:50 -0500

    Added Naive Bayes model

--- sample_dataset_generation.py
+++ sample_dataset_generation.py
@@ -1,4 +1,3 @@
-import json
 import random
 
 input_file = 'Appliances.jsonl'
@@ -8,10 +7,10 @@ with open(input_file, 'r') as f:
     lines = f.readlines()
 
 random.seed(42)
-sampled_lines = random.sample(lines, 50000)
+sampled_lines = random.sample(lines, 100000)
 
 with open(output_file, 'w') as f:
     for line in sampled_lines:
         f.write(line)
 
-print(f"Saved 50,000 random rows to {output_file}.")
+print(f"Saved 100,000 random rows to {output_file}.")
